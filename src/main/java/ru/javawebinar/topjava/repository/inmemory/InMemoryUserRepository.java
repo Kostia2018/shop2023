bc9f6ccd0f24771e47a58334c410953d8bc6e2a4
Константин <konstantinlulakov@gmail.com>
2023-12-14 06:03:43 +0300

    HW_02_add_user

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,63 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
+    private static Map<Integer, User> repository = new ConcurrentHashMap<>();
+
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+
+        return repository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
+
+
+        repository.put(user.getId(), user);
+
         return user;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+
+
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+
+
+        return new ArrayList<>(repository.values());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+
+        User userByEmail = null;
+
+        for (User user : repository.values()) {
+
+            if (user.getEmail().equals(email)) {
+                userByEmail = user;
+            }
+
+
+        }
+
+        return userByEmail;
     }
 }
